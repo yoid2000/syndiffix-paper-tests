4052bdef132ebf24cafb7827743fd3ba50f7f2d2
yoid2000 <paul@francis.com>
2024-03-08 14:05:14 +0100

    Update csv_prep.py

--- csv_prep.py
+++ csv_prep.py
@@ -1,4 +1,5 @@
 import my_utilities as mu
+import zipfile
 import os
 
 ''' Read in csv files, detect datetime fields and set appropriately, 
@@ -10,8 +11,18 @@ csv_path = os.path.join(base_dir, 'original_data_csv')
 pq_path = os.path.join(base_dir, 'original_data_parquet')
 os.makedirs(pq_path, exist_ok=True)
 
+for filename in [filename for filename in os.listdir(csv_path) if filename.endswith('.zip')]:
+    inpath = os.path.join(csv_path, filename)
+    print(f"Found file {inpath}")
+    with zipfile.ZipFile(inpath, 'r') as zip_ref:
+        print(f"Writing to {csv_path}")
+        zip_ref.extractall(csv_path)
+
 for filename in [filename for filename in os.listdir(csv_path) if filename.endswith('.csv')]:
     inpath = os.path.join(csv_path, filename)
+    print(f"Found file {inpath}")
     df = mu.load_csv(inpath)
-    outpath = os.path.join(pq_path, filename)
+    outfile = filename.replace('.csv', '.parquet')
+    outpath = os.path.join(pq_path, outfile)
+    print(f"Writing to {outpath}")
     mu.dump_pq(outpath, df)
