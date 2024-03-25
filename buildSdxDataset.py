4600f766658e6cbc426903d79f4686db2f41d42a
yoid2000 <paul@francis.com>
2024-03-25 09:20:05 +0100

    Update buildSdxDataset.py

--- buildSdxDataset.py
+++ buildSdxDataset.py
@@ -25,8 +25,3 @@ print(job)
 df = mu.load_pq(inPath)
 df_syn = Synthesizer(df[columns]).sample()
 mu.dump_pq(outPath, df_syn)
-
-df_orig = None
-
-from syndiffix.synthesizer import Synthesizer
-df_syn = Synthesizer(df_orig).sample()
