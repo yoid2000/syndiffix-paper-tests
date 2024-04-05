bae8ecdfcae3d7dc1be91b712025762a9d18b449
yoid2000 <paul@francis.com>
2024-04-05 15:30:28 +0200

    update for syndiffix_tools

--- generate_all_sdx_combs_old.py
+++ generate_all_sdx_combs_old.py
@@ -3,7 +3,6 @@ import itertools
 import json
 import my_utilities as mu
 import os
-import syndiffix_tools
 from pathlib import Path
 
 '''
@@ -12,33 +11,41 @@ Also include synthetic dataset with all columns.
 Put the results in allSynCombs.json and create the appropriate slurm scripts.
 '''
 
-DO_LOW_COMBS = True
+DO_LOW_COMBS = False
 
 maxComb = 3
 slurmMem = '320G'
 baseDir = os.environ['SDX_TEST_DIR']
-synDataPath = Path(baseDir, 'synDatasets')
+pqDir = os.path.join(baseDir, 'original_data_parquet')
 
-def updateAllCombs(allCombs, st, cols):
+def updateAllCombs(synFilePath, allCombs, baseName, pqFilePath, cols):
     # check if the file at outPath already exists
-    if st.syn_file_exists(cols):
+    if os.path.exists(synFilePath):
         return
     index = len(allCombs)
     allCombs.append({'index':index,
-                    'synDir':st.get_dir_path_str(),
+                    'synDir':baseName,
+                    'origFile': pqFilePath,
+                    'synPath': synFilePath,
                     'cols': cols})
 
 allCombs = []
-for dir in os.listdir(synDataPath):
-    thisDataPath = Path(synDataPath, dir)
-    st = syndiffix_tools.TablesManager(dir_path=thisDataPath)
-    columns = list(st.df_orig.columns)
+for fileName in [fileName for fileName in os.listdir(pqDir) if fileName.endswith('.parquet')]:
+    baseName = fileName.replace('.parquet','')
+    pqFilePath = os.path.join(pqDir, fileName)
+    print(f"Read file {pqFilePath}")
+    df = mu.load_pq(pqFilePath)
+    columns = list(df.columns)
     if DO_LOW_COMBS:
         for n_dims in range(1,maxComb+1):
             for comb in itertools.combinations(columns,n_dims):
                 cols = sorted(list(comb))
-                updateAllCombs(allCombs, st, cols)
-    updateAllCombs(allCombs, st, columns)
+                synFileName = mu.makeSynFileName(baseName, cols)
+                synFilePath = os.path.join(baseDir, 'synDatasets', baseName,  synFileName + '.parquet')
+                updateAllCombs(synFilePath, allCombs, baseName, pqFilePath, cols)
+    synFileName = baseName + '.all'
+    synFilePath = os.path.join(baseDir, 'synDatasets', baseName,  synFileName + '.parquet')
+    updateAllCombs(synFilePath, allCombs, baseName, pqFilePath, columns)
 print(f"Made {len(allCombs)} combinations")
 allCombsPath = os.path.join(baseDir, 'allSynCombs.json')
 with open(allCombsPath, 'w') as f:
