2a24f4f06ad3258a0b96c57e9df7a0819fb614d9
yoid2000 <paul@francis.com>
2024-04-10 23:28:23 +0200

    Update exact_count.py

--- exact_count.py
+++ exact_count.py
@@ -59,7 +59,7 @@ for c in num_cols:
         for i in [0,1]:
             noisy_counts[i].append(df_syn[df_syn[col0] == i].shape[0])
         precision[ckey]['1dim'].append(get_precision(noisy_counts, exact_counts))
-        print(f"\b{this_try}.1", flush=True)
+        print(f"{this_try}.1", flush=True)
         noisy_counts = [[],[]]
         for col in cols_without_col0:
             df_syn = Synthesizer(df[[col0,col]]).sample()
@@ -67,7 +67,7 @@ for c in num_cols:
             for i in [0,1]:
                 noisy_counts[i].append(df_syn[df_syn[col0] == i].shape[0])
         precision[ckey]['2dim'].append(get_precision(noisy_counts, exact_counts))
-        print(f"\b{this_try}.2", flush=True)
+        print(f"{this_try}.2", flush=True)
         noisy_counts = [[],[]]
         for comb in itertools.combinations(cols_without_col0, 2):
             cols = [col0] + list(comb)
@@ -76,7 +76,7 @@ for c in num_cols:
             for i in [0,1]:
                 noisy_counts[i].append(df_syn[df_syn[col0] == i].shape[0])
         precision[ckey]['3dim'].append(get_precision(noisy_counts, exact_counts))
-        print(f"\b{this_try}.3", flush=True)
+        print(f"{this_try}.3", flush=True)
     precision[ckey]['1dim'] = statistics.mean(precision[ckey]['1dim'])
     precision[ckey]['2dim'] = statistics.mean(precision[ckey]['2dim'])
     precision[ckey]['3dim'] = statistics.mean(precision[ckey]['3dim'])
