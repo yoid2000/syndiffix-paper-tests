686e5444de3d1bc09b7b7ceb1d0833bf790807d8
yoid2000 <paul@francis.com>
2024-04-20 18:40:26 +0200

    expand exact_count.py

--- exact_count.py
+++ exact_count.py
@@ -5,6 +5,7 @@ import statistics
 import itertools
 import json
 from syndiffix import Synthesizer
+import os
 
 '''
 This program tests the ability for an attacker to determine the exact count of
@@ -22,8 +23,12 @@ datasets, we have 200/8=25 expected rows per combination, which is well above th
 suppression limit
 '''
 
-num_rows = 300
+num_other_rows = 50
 num_cols = [20, 40, 80, 160]
+num_vals = [2, 4, 8]
+dims = [1,2,3]
+num_rows = [10, 20, 40]
+base_rows_per_val = 20
 # We want more runs for lower dimension data because each run has fewer samples
 # We need more runs to get significant results (3-column measures take a long time
 # to run)
@@ -33,14 +38,13 @@ THREE_COLS = True
 attack_keys = ['1dim', '2dim', '3dim']
 
 # read command line arguments
-attack = 'do_simple'
+do_attack_num = None
+make_slurm = False
 if len(sys.argv) > 1:
-    attack = sys.argv[1]
-
-if attack == 'do_simple':
-    outFile = 'exact_count_precision_simple.json'	
-elif attack == 'do_adjust':
-    outFile = 'exact_count_precision_adjust.json'
+    if sys.argv[1] == 'slurm':
+        make_slurm = True
+    else:
+        do_attack_num = int(sys.argv[1])
 
 def print_progress_wheel(wheel):
     print(next(wheel) + '\b', end='', flush=True)
@@ -50,128 +54,120 @@ def progress_wheel():
     while True:
         yield next(wheel)
 
-def get_precision(noisy_counts, exact_counts):
+def get_precision(noisy_counts, exact_count, true_row_count):
     num_correct = 0
-    # Assume that we can determine the exact number of rows by taking
-    # average from all tables
-    true_row_count = sum(exact_counts)
-    # We know that the sum of noisy counts should be equal to the true row 
-    # count, so if not we adjust to make it so
-    if attack == 'do_adjust':
-        adjust_target = np.random.randint(0,2)
-        for s in range(len(noisy_counts[0])):
-            adjustment = true_row_count - (noisy_counts[0][s] + noisy_counts[1][s])
-            while abs(adjustment) > 1:
-                # If we need to adjust by two, then we adjust both counts
-                if adjustment > 0:
-                    for i in [0,1]:
-                        noisy_counts[i][s] += 1
-                    adjustment -= 2
-                else:
-                    for i in [0,1]:
-                        noisy_counts[i][s] -= 1
-                    adjustment += 2
-            # Now we need to adjust by -1, 0, or 1
-            if adjustment == 1:
-                noisy_counts[adjust_target][s] += 1
-            elif adjustment == -1:
-                noisy_counts[adjust_target][s] -= 1
-    guesses = [round(sum(noisy_counts[i])/len(noisy_counts[i])) for i in [0,1]]
-    guesses_correct = []
-    for i in [0,1]:
-        guess = guesses[i]
-        if guess == exact_counts[i]:
-            guesses_correct.append(1)
-        else:
-            guesses_correct.append(0)
-    errors = [abs(guesses[i] - exact_counts[i]) for i in [0,1]]
-    return {'correct': guesses_correct, 'guessed': guesses, 'exact': exact_counts, 'errors': errors}
+    # true_row_count assumes that we can determine the exact number of
+    # rows by taking average from all tables
+    guess = round(sum(noisy_counts)/len(noisy_counts))
+    if guess == exact_count:
+        correct = 1
+    else:
+        correct = 0
+    error = abs(guess - exact_count)
+    return {'correct': correct, 'guessed': guess, 'exact': exact_count, 'error': error}
 
-def summarize_and_dump(precision, ckey):
-    precision[ckey]['correct_averages']['1dim'] = statistics.mean(precision[ckey]['scores']['1dim'])
-    precision[ckey]['error_averages']['1dim'] = statistics.mean(precision[ckey]['errors']['1dim'])
-    if len(precision[ckey]['errors']['1dim']) > 1:
-        precision[ckey]['error_std_devs']['1dim'] = statistics.stdev(precision[ckey]['errors']['1dim'])
-    precision[ckey]['samples']['1dim'] = len(precision[ckey]['scores']['1dim'])
-    if TWO_COLS:
-        precision[ckey]['correct_averages']['2dim'] = statistics.mean(precision[ckey]['scores']['2dim'])
-        precision[ckey]['error_averages']['2dim'] = statistics.mean(precision[ckey]['errors']['1dim'])
-        if len(precision[ckey]['errors']['2dim']) > 1:
-            precision[ckey]['error_std_devs']['2dim'] = statistics.stdev(precision[ckey]['errors']['2dim'])
-        precision[ckey]['samples']['2dim'] = len(precision[ckey]['scores']['2dim'])
-    if THREE_COLS:
-        precision[ckey]['correct_averages']['3dim'] = statistics.mean(precision[ckey]['scores']['3dim'])
-        precision[ckey]['error_averages']['3dim'] = statistics.mean(precision[ckey]['errors']['1dim'])
-        if len(precision[ckey]['errors']['3dim']) > 1:
-            precision[ckey]['error_std_devs']['3dim'] = statistics.stdev(precision[ckey]['errors']['3dim'])
-        precision[ckey]['samples']['3dim'] = len(precision[ckey]['scores']['3dim'])
-    print(precision)
+def make_df(num_val, num_col, num_row, this_try):
+    np.random.seed(this_try + (num_col * 100) + (num_val * 1000) + (num_row * 10000))
+    data = {}
+    num_rows_base = base_rows_per_val * num_val
+    num_rows_total = (base_rows_per_val * num_val) + num_row
+    for i in range(num_col):
+        if i == 0:
+            # The 0th column has the value 1
+            data[f'col{this_try}_{i}'] = [1] * num_rows_base
+            # Now add the target rows, with value 0
+            for j in range(num_row):
+                data[f'col{this_try}_{i}'].append(0)
+        else:
+            # The remaining columns each have num_val distinct values, uniformly randomly assigned
+            values = np.random.choice(range(num_val), num_rows_total)
+            data[f'col{this_try}_{i}'] = values
+    return pd.DataFrame(data)
+
+def get_col_combs(df, col0, dim):
+    cols_without_col0 = [col for col in df.columns if col != col0]
+    col_combs = []
+    if dim == 1:
+        col_combs.append([col0])
+    if dim == 2:
+        for col in cols_without_col0:
+            col_combs.append([col0,col])
+    elif dim == 3:
+        for comb in itertools.combinations(cols_without_col0, 2):
+            col_combs.append([col0] + list(comb))
+    return col_combs
+
+def do_attack(num_val, num_col, dim, num_row):
+    '''
+    num_val: number of distinct values in each other column
+    num_col: number of columns
+    dim: number of dimensions of the attack
+    num_row: number of rows of the value being predicted
+    '''
+    prec = {
+        'num_val': num_val,
+        'num_col': num_col,
+        'dim': dim,
+        'num_row': num_row,
+        'correct_averages': 0,
+        'error_averages': 0,
+        'error_std_devs': 0,
+        'samples': 0,
+        'errors': [],
+        'scores': [],
+        'results': [],
+    }
+    if dim == 1:
+        num_tries = min_samples
+    elif dim == 2:
+        num_tries = max(20, min_samples / (num_col - 1))
+    elif dim == 3:
+        num_tries = max(20, min_samples / (((num_col-1) * (num_col-2)) / 2))
+    for this_try in range(num_tries):
+        # set the seed for np.random
+        df = make_df(num_val, num_col, num_row, this_try)
+        col0 = f'col{this_try}_0'
+        exact_count = df[df[col0] == 0].shape[0]
+        col_combs = get_col_combs(df, col0, dim)
+        # get the count of the target value 0 for col0 in df
+        noisy_counts = []
+        for col_comb in col_combs:
+            df_syn = Synthesizer(df[col_comb]).sample()
+            noisy_counts.append(df_syn[df_syn[col0] == 0].shape[0])
+        result = get_precision(noisy_counts, exact_count, df.shape[0])
+        prec['results'].append(result)
+        prec['scores'].append(result['correct'])
+        prec['errors'].append(result['error'])
+    prec['correct_averages'] = statistics.mean(prec['scores'])
+    prec['error_averages'] = statistics.mean(prec['errors'])
+    if len(prec['errors']) > 1:
+        prec['error_std_devs'] = statistics.stdev(prec['errors'])
+    else:
+        prec['error_std_devs'] = 0
+    prec['samples'] = len(prec['scores'])
     # dump precision as a json file
-    with open(outFile, 'w') as f:
-        json.dump(precision, f, indent=4)
+    # make directory 'exact_count_results' if it does not exist
+    if not os.path.exists('exact_count_results'):
+        os.makedirs('exact_count_results')
+    file_name = f'v{num_val}.c{num_col}.d{dim}.r{num_row}.json'
+    # make a path for file_name in 'exact_count_results' directory
+    file_path = os.path.join('exact_count_results', file_name)
+    with open(file_path, 'w') as f:
+        json.dump(prec, f, indent=4)
 
 wheel = progress_wheel()
-precision = {}
-# get both the index and the value for the list num_cols
-
-for cix, c in enumerate(num_cols):
-    ckey = f"{c} cols"
-    precision[ckey] = {
-                    'correct_averages': {'1dim': 0, '2dim': 0, '3dim': 0},
-                    'error_averages': {'1dim': 0, '2dim': 0, '3dim': 0},
-                    'error_std_devs': {'1dim': 0, '2dim': 0, '3dim': 0},
-                    'samples': {'1dim': 0, '2dim': 0, '3dim': 0},
-                    'errors': {'1dim': [], '2dim': [], '3dim': []},
-                    'scores': {'1dim': [], '2dim': [], '3dim': []},
-                    'results': {'1dim': [], '2dim': [], '3dim': []},
-                    }
-    num_correct = [0,0]
-    samples_per_2col = max(20, min_samples / (c - 1))
-    samples_per_3col = max(20, min_samples / (((c-1) * (c-2)) / 2))
-    for this_try in range(min_samples):
-        # set the seed for np.random
-        np.random.seed(this_try + (c * 1000000))
-        df = pd.DataFrame(np.random.randint(0, 2, size=(num_rows, c)), 
-                          columns=[f'col{this_try}_{i}' for i in range(c)])
-        true_row_count = df.shape[0]
-        col0 = f'col{this_try}_0'
-        cols_without_col0 = [col for col in df.columns if col != col0]
-        # get the count of each value for col0 in df
-        exact_counts = [0,0]
-        for i in [0,1]:
-            exact_counts[i] = df[df[col0] == i].shape[0]
-        df_syn = Synthesizer(df[[col0]]).sample()
-        noisy_counts = [[],[]]
-        for i in [0,1]:
-            noisy_counts[i].append(df_syn[df_syn[col0] == i].shape[0])
-        results = get_precision(noisy_counts, exact_counts)
-        precision[ckey]['results']['1dim'].append(results)
-        precision[ckey]['scores']['1dim'] += results['correct']
-        precision[ckey]['errors']['1dim'] += results['errors']
-        if TWO_COLS and this_try <= samples_per_2col:
-            noisy_counts = [[],[]]
-            for col in cols_without_col0:
-                df_syn = Synthesizer(df[[col0,col]]).sample()
-                #print_progress_wheel(wheel)
-                for i in [0,1]:
-                    noisy_counts[i].append(df_syn[df_syn[col0] == i].shape[0])
-            results = get_precision(noisy_counts, exact_counts)
-            precision[ckey]['results']['2dim'].append(results)
-            precision[ckey]['scores']['2dim'] += results['correct']
-            precision[ckey]['errors']['2dim'] += results['errors']
-            #print(f"{c}-{this_try}.2 (of {samples_per_2col})", flush=True)
-        if THREE_COLS and this_try <= samples_per_3col:
-            noisy_counts = [[],[]]
-            for comb in itertools.combinations(cols_without_col0, 2):
-                cols = [col0] + list(comb)
-                df_syn = Synthesizer(df[cols]).sample()
-                #print_progress_wheel(wheel)
-                for i in [0,1]:
-                    noisy_counts[i].append(df_syn[df_syn[col0] == i].shape[0])
-            results = get_precision(noisy_counts, exact_counts)
-            precision[ckey]['results']['3dim'].append(results)
-            precision[ckey]['scores']['3dim'] += results['correct']
-            precision[ckey]['errors']['2dim'] += results['errors']
-            print(f"{c}-{this_try}.3 (of {samples_per_3col})", flush=True)
-            summarize_and_dump(precision, ckey)
-    summarize_and_dump(precision, ckey)
+attack_num = 0
+for num_val in num_vals:
+    for num_col in num_cols:
+        for dim in dims:
+            for num_row in num_rows:
+                if make_slurm is False and (do_attack_num is None or attack_num == do_attack_num):
+                    do_attack(num_val, num_col, dim, num_row)
+                attack_num += 1
+if make_slurm:
+    with open('exact_count_slurm.sh', 'w') as f:
+        f.write('#!/bin/bash\n')
+        f.write('#SBATCH --time=0-01:00\n')
+        f.write('#SBATCH --mem=10G\n')
+        f.write('#SBATCH --array=0-' + str(attack_num - 1) + '\n')
+        f.write('python exact_count.py $SLURM_ARRAY_TASK_ID\n')
