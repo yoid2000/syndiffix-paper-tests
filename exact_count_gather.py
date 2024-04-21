b8fc8f40b01c399c7770b3f7bf282b99d43f50da
yoid2000 <paul@francis.com>
2024-04-21 11:28:45 +0200

    Update exact_count_gather.py

--- exact_count_gather.py
+++ exact_count_gather.py
@@ -1,20 +1,19 @@
 import json
 import os
 
-# Step 1: Read in all of the .json files in directory ./exact_count_results
 json_files = [pos_json for pos_json in os.listdir("./exact_count_results") if pos_json.endswith('.json')]
 
 data_list = []
 
-# Step 2: For each file, copy the specified items into a dict
 for file in json_files:
     with open(f"./exact_count_results/{file}", "r") as json_file:
         data = json.load(json_file)
         data_dict = {key: data[key] for key in ("num_val", "num_col", "dim", "num_row", "correct_averages", "error_averages", "error_std_devs", "samples")}
         data_list.append(data_dict)
+# create directory 'results' if it doesn't exist
+if not os.path.exists("results"):
+    os.makedirs("results")
+out_path = os.path.join("results", "exact_count_data.json")
 
-# Step 3: Put the dict into a list (already done in step 2)
-
-# Step 4: Write the list of dicts into a json file in the local directory called exact_count_data.json
-with open("exact_count_data.json", "w") as outfile:
+with open(out_path, "w") as outfile:
     json.dump(data_list, outfile, indent=4)
