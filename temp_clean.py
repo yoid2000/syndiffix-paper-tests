0892dc63e5a4a4a7343e2f3905e4135a275ad8f5
yoid2000 <paul@francis.com>
2024-04-26 09:45:14 +0200

    Update temp_clean.py

--- temp_clean.py
+++ temp_clean.py
@@ -6,12 +6,13 @@ json_files = [pos_json for pos_json in os.listdir("./exact_count_results") if po
 data = []
 
 for file in json_files:
-    with open(f"./exact_count_results/{file}", "r") as json_file:
+    filepath = os.path.join("exact_count_results", file)
+    with open(filepath, "r") as json_file:
         print(file)
         dat1 = json.load(json_file)
         for thing in dat1:
             if thing['num_col'] != 5 and thing['num_col'] != 6:
                 data.append(thing)
     # write data to json file
-    with open(f"./exact_count_results/{file}", "w") as json_file:
+    with open(filepath, "w") as json_file:
         json.dump(data, json_file)
