b4d27d534c8973b5fb5e1493ced12b8a74c0c4ec
yoid2000 <paul@francis.com>
2024-05-18 09:34:55 +0200

    plots

--- suppress_threshold_attack.py
+++ suppress_threshold_attack.py
@@ -167,8 +167,8 @@ def do_plots():
     num_bins = 80
     df_temp = X_test_all.copy()
     df_temp['bin'] = pd.cut(df_temp['pi_fl'], bins=num_bins)
-
     df_bin = df_temp.groupby('bin', observed=True).size().reset_index(name='count')
+
     df_bin['pi_fl_mid'] = df_bin['bin'].apply(lambda x: (x.right + x.left) / 2)
     for column in df_temp.columns:
         # If the dtype is numeric, compute the mean for each bin
@@ -203,8 +203,8 @@ def do_plots():
 
     # Create a basic scatterplot from the bins
     plt.figure(figsize=(7, 3.5))
-    plt.scatter(df_bin['frac_perfect'], df_bin['pi_fl_mid'], c=df_bin['frac_tar_avg'], cmap='viridis', marker='o', label='Attack contitions\nhappen to exist')
-    plt.scatter(df_bin['frac_capt'], df_bin['pi_fl_mid'], c=df_bin['frac_tar_avg'], cmap='viridis', marker='x', label='Attack specific person\nand target')
+    plt.scatter(df_bin['frac_perfect'], df_bin['pi_fl_mid'], c=df_bin['frac_tar_avg'], cmap='viridis', s=df_bin['count'], marker='o', label='Attack contitions\nhappen to exist')
+    plt.scatter(df_bin['frac_capt'], df_bin['pi_fl_mid'], c=df_bin['frac_tar_avg'], cmap='viridis', s=df_bin['count'], marker='x', label='Attack specific person\nand target')
     plt.scatter(df_bin['frac_perfect'], df_bin['pi_fl_mid'], c=df_bin['frac_tar_avg'], cmap='viridis')
     plt.colorbar(label='Fraction of rows with target value')
     plt.xscale('log')
