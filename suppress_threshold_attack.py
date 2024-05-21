ab35ffc2a90e80bdbe17ffb856a19a8697813eb3
yoid2000 <paul@francis.com>
2024-05-21 17:47:39 +0200

    Update suppress_threshold_attack.py

--- suppress_threshold_attack.py
+++ suppress_threshold_attack.py
@@ -287,7 +287,7 @@ def do_plots():
     print(f"Count of rows where pi_fl == 1: {count_pi_fl_1}")
 
     # Make a scatterplot of pi_fl vs coverage
-    num_bins = 80
+    num_bins = 40
     df_temp = X_test_all.copy()
     df_temp['bin'] = pd.cut(df_temp['pi_fl'], bins=num_bins)
     df_bin = df_temp.groupby('bin', observed=True).size().reset_index(name='count')
