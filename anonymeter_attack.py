63a5496becdb02e825056c8081eabfa028c7508d
yoid2000 <paul@francis.com>
2024-05-28 16:42:37 +0200

    initial anonymeter_attack.py

---
+++ anonymeter_attack.py
@@ -0,0 +1,101 @@
+from anonymeter.evaluators import InferenceEvaluator
+
+if 'SDX_TEST_DIR' in os.environ:
+    base_path = os.getenv('SDX_TEST_DIR')
+else:
+    base_path = os.getcwd()
+if 'SDX_TEST_CODE' in os.environ:
+    code_path = os.getenv('SDX_TEST_CODE')
+    from syndiffix_tools.tables_manager import TablesManager
+else:
+    code_path = None
+syn_path = os.path.join(base_path, 'synDatasets')
+attack_path = os.path.join(base_path, 'anonymeter_attacks')
+os.makedirs(attack_path, exist_ok=True)
+num_attacks = 20000
+
+def make_config():
+    ''' I want to generate num_attacks attacks. Each attack will be on a given secret
+    column in a given table. I will run multiple of these attacks per secret/table if
+    necessary.
+    '''
+    # Initialize attack_jobs
+    attack_jobs = []
+
+    # Loop over each directory name in syn_path
+    for dir_name in os.listdir(syn_path):
+        # Create a TablesManager object with the directory path
+        tm = TablesManager(dir_path=os.path.join(syn_path, dir_name, 'anonymeter'))
+        columns = list(tm.df_orig.columns)
+        pid_cols = tm.get_pid_cols()
+        if len(pid_cols) > 0:
+            # We can't really run the attack on time-series data
+            continue
+        for secret in columns:
+            attack_jobs.append({
+                'index': len(attack_jobs),
+                'dir_name': dir_name,
+                'secret': col1,
+            })
+    while len(attack_jobs < num_attacks):
+        attack_jobs.extend(attack_jobs)
+    # randomize the order in which the attack_jobs are run
+    random.shuffle(attack_jobs)
+    # remove any extra attack_jobs
+    attack_jobs = attack_jobs[:num_attacks]
+
+    # Write attack_jobs into a JSON file
+    with open(os.path.join(attack_path, 'attack_jobs.json'), 'w') as f:
+        json.dump(attack_jobs, f, indent=4)
+
+    exe_path = os.path.join(code_path, 'anonymeter_attack.py')
+    venv_path = os.path.join(base_path, 'sdx_venv', 'bin', 'activate')
+    slurm_dir = os.path.join(attack_path, 'slurm_out')
+    os.makedirs(slurm_dir, exist_ok=True)
+    slurm_out = os.path.join(slurm_dir, 'out.%a.out')
+    num_jobs = len(attack_jobs) - 1
+    # Define the slurm template
+    slurm_template = f'''#!/bin/bash
+#SBATCH --job-name=suppress_attack
+#SBATCH --output={slurm_out}
+#SBATCH --time=7-0
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=1
+#SBATCH --array=0-{num_jobs}
+arrayNum="${{SLURM_ARRAY_TASK_ID}}"
+source {venv_path}
+python {exe_path} $arrayNum
+'''
+    # write the slurm template to a file attack.slurm
+    with open(os.path.join(attack_path, 'attack.slurm'), 'w') as f:
+        f.write(slurm_template)
+
+def run_attack(job_num):
+    pass
+
+def gather(instances_path):
+    pass
+
+def do_plots():
+    pass
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", help="'config' to run make_config(), or an integer to run run_attacks()")
+    args = parser.parse_args()
+
+    if args.command == 'config':
+        make_config()
+    elif args.command == 'gather':
+        gather(instances_path=os.path.join(attack_path, 'instances'))
+    elif args.command == 'plots':
+        do_plots()
+    else:
+        try:
+            job_num = int(args.command)
+            run_attack(job_num)
+        except ValueError:
+            print(f"Unknown command: {args.command}")
+
+if __name__ == "__main__":
+    main()
