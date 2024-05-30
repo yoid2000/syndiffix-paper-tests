0da74085c91cc24ed2edc3e23bb2ac5f286d45e8
yoid2000 <paul@francis.com>
2024-05-30 14:38:46 +0200

    Update anonymeter_attack.py

--- anonymeter_attack.py
+++ anonymeter_attack.py
@@ -33,7 +33,7 @@ num_runs_per_attack = 100
 max_subsets = 200
 
 def find_most_frequent_value(lst, fraction):
-    if len(list) == 0:
+    if len(lst) == 0:
         return None
 
     # Count the frequency of each value in the list
