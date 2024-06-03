65e6ace5268640606d3d79e245ce68ae9131c36d
yoid2000 <paul@francis.com>
2024-06-03 16:16:47 +0200

    Update anonymeter_mods.py

--- anonymeter_mods.py
+++ anonymeter_mods.py
@@ -296,8 +296,6 @@ def run_anonymeter_attack(
         print(f"no matching rows")
         pp.pprint(ans)
         sys.exit(1)
-    if ans['modal_count'] == 2:
-        pp.pprint(ans)
     return ans
     #return evaluate_inference_guesses(guesses=guesses, secrets=targets[secret], regression=regression).sum()
 
