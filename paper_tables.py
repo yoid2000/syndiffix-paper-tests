3b0ab3ddd8fa7b0a7b431d08c5a512fc69bf1f69
yoid2000 <paul@francis.com>
2024-06-11 14:56:03 +0200

    Create paper_tables.py

---
+++ paper_tables.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import itertools
+import json
+import my_utilities as mu
+import os
+import sys
+from syndiffix_tools.tables_manager import TablesManager
+from pathlib import Path
+
+'''
+Split the datasets into control and training parts
+'''
+
+baseDir = os.environ['SDX_TEST_DIR']
+synDataPath = Path(baseDir, 'synDatasets')
+
+for dir in os.listdir(synDataPath):
+    thisDataPath = Path(synDataPath, dir)
+    tm = TablesManager(dir_path=thisDataPath)
+    pid_cols = tm.get_pid_cols()
+    if tm.orig_meta_data['column_classes'][job['secret']] == 'continuous':
+        pass
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", help="'config' to run make_config(), or an integer to run run_attacks()")
+    args = parser.parse_args()
+
+    if args.command == 'datasets':
+        make_datasets()
+
+if __name__ == "__main__":
+    main()
